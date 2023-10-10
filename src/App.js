e0e1e6c8d6f2caaa9712eea27c70d4781a37ae61
Alex6n <hmahh30@gmail.com>
2023-10-10 21:52:42 +0300

    Password Generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import '@mantine/core/styles.css';
+import { MantineProvider, Center } from '@mantine/core';
+import Generator from './Components/Generator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() { 
+  return ( 
+    <MantineProvider defaultColorScheme="dark">
+      <Center> 
+      <Generator />
+      </Center>
+    </MantineProvider>
+  ); 
+} 
 
 export default App;
